736cca74acb02d9dd6dc2230132c21e73dec3a51
Mohamed Artan <99888102+mdartan@users.noreply.github.com>
2022-02-17 17:10:31 +0300

    Delete task/app directory

--- task/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import todo
-# Register your models here.
-
-admin.site.register(todo)
-class TodoAdmin(admin.ModelAdmin):
-    list_display = ('User', 'ToDo', 'Status', 'Created',)
-    ordering = ('ToDo',)
-    search_fields = ('User','ToDo', 'Status',)
